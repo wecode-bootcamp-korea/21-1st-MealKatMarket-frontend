53ad4b954a24ab8ed3e57c17a19933f29a88e974
강준희 <junheekang@jeongsohyeon-ui-MacBook-Pro.local>
2021-06-08 15:19:31 +0900

    Add: Header 추가

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,8 +2,47 @@ import React from 'react';
 import './Header.scss';
 
 class Header extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      clicked: false,
+    };
+  }
+
   render() {
-    return <div>Header</div>;
+    return (
+      <div className="header-wrapper">
+        <div className="header-container">
+          <div className="logo-contatiner">
+            <button className="logo-button">
+              <a herf="#" className="logo-link">
+                <img className="logo" src="/logo3.png" alt="logo" />
+              </a>
+            </button>
+          </div>
+          <img src="Search.svg"></img>
+        </div>
+        <div className="header-top-menu">
+          <ul className="header-top-menu-list">
+            <li>
+              <a href="#" className="menu-link">
+                쿠캣추천
+              </a>
+            </li>
+            <li>
+              <a href="#" className="menu-link">
+                베스트
+              </a>
+            </li>
+            <li>
+              <a href="#" className="menu-link">
+                런칭특가
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
   }
 }
 
