b9bf9f458d1402c3dd2d1f88ad47bf0aabea91c9
Jay Park <devpark.0x1c@gmail.com>
2021-06-08 15:24:38 +0900

    Add: 공통 변수 추가

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header/Header';
+import Search from './components/Search/Search';
 import Main from './pages/Main/Main';
 import Register from './pages/Register/Register';
 
@@ -12,6 +13,7 @@ class Routes extends React.Component {
           <Route exact path="/" component={Main} />
           <Route exact path="/header" component={Header} />
           <Route exact path="/register" component={Register} />
+          <Route exact path="/search" component={Search} />
         </Switch>
       </Router>
     );
