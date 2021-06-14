10cb5045bfa018f53504be1f938d4b1d5e306b60
강준희 <junheekang@jeongsohyeon-ui-MacBook-Pro.local>
2021-06-14 17:24:04 +0900

    Modify: 브랜치 변경

--- src/Routes.js
+++ src/Routes.js
@@ -8,7 +8,7 @@ import Main from './pages/Main/Main';
 import Register from './pages/Register/Register';
 import Login from './pages/Login/Login';
 import Product from './pages/Product/Product';
-import ProductCard from './components/ProductCard/ProductCard';
+import Card from './components/ProductCard/Card';
 import BottomNav from './components/BottomNav/BottomNav';
 import Bargain from './pages/Bargain/Bargain';
 import Timer from './components/Timer/Timer';
@@ -28,7 +28,7 @@ class Routes extends React.Component {
           <Route exact path="/footer" component={Footer} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/product" component={Product} />
-          <Route exact path="/productcard" component={ProductCard} />
+          <Route exact path="/productcard" component={Card} />
           <Route exact path="/bottomnav" component={BottomNav} />
           <Route exact path="/bargain" component={Bargain} />
           <Route exact path="/timer" component={Timer} />
--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -1,50 +1,112 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Product.scss';
+import Card from '../../components/ProductCard/Card';
 
 class Product extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showDropdown: false,
+      CARD_DATA: [],
+    };
+    this.showDropdown = this.showDropdown.bind(this);
+  }
+
+  showDropdown() {
+    this.setState(prevState => ({
+      showDropdown: !prevState.showDropdown,
+    }));
+  }
+
+  componentDidUpdate() {
+    console.log(this.state.CARD_DATA.result);
+  }
+
+  componentDidMount() {
+    fetch('http://10.58.7.47:8000/products', {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          CARD_DATA: data,
+        });
+      });
+  }
   render() {
     return (
       <div className="product-container">
         <div className="background">
           <ul className="swiper-wrapper">
             <li className="menu-wrapper">
-              <Link to="/" className="menu">
+              <Link to="/products" className="menu">
                 전체
               </Link>
             </li>
             <li>
-              <Link to="/" className="menu-2">
+              <Link to="/products?category-id=1" className="menu-2">
                 간편요리
               </Link>
             </li>
             <li>
-              <Link to="/" className="menu">
+              <Link to="/products?category-id=2" className="menu">
                 밥류
               </Link>
             </li>
             <li>
-              <Link to="/" className="menu">
+              <Link to="/products?category-id=3" className="menu">
                 면류
               </Link>
             </li>
             <li>
-              <Link to="/" className="menu">
+              <Link to="/products?category-id=4" className="menu">
                 반찬
               </Link>
             </li>
             <li>
-              <Link to="/" className="menu">
+              <Link to="/products?category-id=5" className="menu">
                 간식
               </Link>
             </li>
             <li>
-              <Link to="/" className="menu">
+              <Link to="/products?category-id=6" className="menu">
                 음료
               </Link>
             </li>
           </ul>
-          <ul></ul>
+          <div className="filter-button">
+            <button onClick={this.showDropdown}>
+              인기상품순
+              <img src="/icon/swap.svg" alt="swap-icon" />
+            </button>
+            {this.state.showDropdown && (
+              <ul className="filter-dropdown">
+                <li>
+                  <Link className="link">출시일순</Link>
+                </li>
+                <li>
+                  <Link className="link">인기상품순</Link>
+                </li>
+                <li>
+                  <Link className="link">후기많은순</Link>
+                </li>
+                <li>
+                  <Link className="link">낮은가격순</Link>
+                </li>
+                <li>
+                  <Link className="link">높은가격순</Link>
+                </li>
+              </ul>
+            )}
+          </div>
+          <ul className="product-list">
+            {this.state.CARD_DATA.result &&
+              this.state.CARD_DATA.result.map((card, idx) => (
+                <Card key={idx} card={card} />
+              ))}
+          </ul>
         </div>
       </div>
     );
