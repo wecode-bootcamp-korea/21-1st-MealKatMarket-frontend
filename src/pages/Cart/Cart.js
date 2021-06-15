1618544045ef7e7fc583cb9052a9ac65b3b67cd3
Jay Park <devpark.0x1c@gmail.com>
2021-06-15 10:47:49 +0900

    Add: 가격 항목 창 추가

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -3,19 +3,28 @@ import { Link } from 'react-router-dom';
 import './Cart.scss';
 
 class Cart extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentItems: [],
+    };
+  }
+
   goBack = () => {
     this.props.history.goBack();
   };
 
   render() {
+    const { currentItems } = this.state;
+
     return (
-      <div className="cart-container">
+      <div className="Cart">
         <div className="background">
           <header>
             <span className="left-arrow" onClick={this.goBack}>
               <img alt="return back icon" src="/icon/LeftArrow.svg" />
             </span>
-            <span class="header-title">카트</span>
+            <span className="header-title">카트</span>
             <span className="home-icon">
               <Link to="/">
                 <img alt="Home Icon" src="/icon/Home_Black.svg" />
@@ -26,21 +35,41 @@ class Cart extends React.Component {
             <img alt="banner" src="/images/cart_banner.png" />
           </section>
           <section className="cart-goods-container">
-            <section className="goods-exist">
-              <button className="add-address-button">배송지 입력</button>
-            </section>
-            <section className="goods-empty">
-              <img alt="cart icon" src="/icon/Buy_shadow.svg" />
-              <p>카트가 비어있어요</p>
-              <Link className="add-cart" to="/">
-                채우러 가기
-              </Link>
-            </section>
+            {currentItems.length > 0 ? (
+              <section className="goods-exist">
+                <button className="add-address-button">배송지 입력</button>
+              </section>
+            ) : (
+              <section className="goods-empty">
+                <img alt="cart icon" src="/icon/Buy_shadow.svg" />
+                <p>카트가 비어있어요</p>
+                <Link className="add-cart" to="/">
+                  채우러 가기
+                </Link>
+              </section>
+            )}
           </section>
         </div>
         <section className="current-popular-goods-container">
           <p>실시간 베스트 상품</p>
         </section>
+        {currentItems.length > 0 && (
+          <section className="calculate-container">
+            <section className="total-product-value">
+              <span className="value-text">총 상품 금액</span>
+              <span className="value-number">7,500원</span>
+            </section>
+            <section className="total-shipping-value">
+              <span className="value-text">총 배송비</span>
+              <span className="value-number">3,000원</span>
+            </section>
+            <div className="divide-line"></div>
+            <section className="total-value">
+              <span className="total-text">결제예정금액</span>
+              <span className="total-number">10,500원</span>
+            </section>
+          </section>
+        )}
       </div>
     );
   }
